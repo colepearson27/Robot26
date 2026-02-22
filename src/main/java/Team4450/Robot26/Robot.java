c2abb96e5954b4b6484f22ca9b22ab89ff7a23c4
skyhirsch <skyhirsch17@gmail.com>
2026-02-22 15:31:52 -0800

    no adstar

--- src/main/java/Team4450/Robot26/Robot.java
+++ src/main/java/Team4450/Robot26/Robot.java
@@ -39,7 +39,6 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void robotInit() {
-        Pathfinding.setPathfinder(new LocalADStar());
         try {
             robot = this;   // Stored in Constants.
 
